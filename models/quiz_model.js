5a9c5bd26f822720fe2dccbce170d39a53df166d
OscarTrujillo <oscartrujillo94@gmail.com>
2015-11-25 22:18:25 +0000

    creada estructura de clases Pregunta()

--- models/quiz_model.js
+++ models/quiz_model.js
@@ -17,49 +17,70 @@ function Respuesta(answer){
   
 }
 
+function Pregunta(quest){
+      this.quest = quest; 
+      
+      this.pregunta = quest
+}
+
+
+Pregunta.prototype = new Pregunta();
+Pregunta.prototype.constructor = Pregunta;
+
+function PreguntaCorta(quest){
+    Pregunta.call(this, quest);
+    this.intro = "<input type='text' name='respuesta' placeholder='Responda aquí'>";
+}
 
 
 
+Pregunta.prototype = new Pregunta();
+Pregunta.prototype.constructor = Pregunta;
+
+function PreguntaLarga(quest){
+    Pregunta.call(this, quest);
+    this.intro = "<textarea type='text' name='respuesta' placeholder='Responda aquí'></textarea>";
+}
 
 
 
 function Quiz() {
   AbstractQuiz.call(this);
   this.q.push(
-    { pregunta: '¿Capital de Italia?',
+    { pregunta: new PreguntaCorta('¿Capital de Italia?'),
       respuesta: new Respuesta(/^\s*roma\s*$/i)
     },
-     { pregunta: '¿Serie de televisión más vista en el mundo?',
-      respuesta: new Respuesta(/^\s*house\s*$/i)
+     { pregunta: new PreguntaCorta('¿Serie de televisión más vista en el mundo?'),
+       respuesta: new Respuesta(/^\s*house\s*$/i)
       
     },
-    { pregunta: 'Nombre de la montaña más alta del mundo:',
+    { pregunta: new PreguntaCorta('Nombre de la montaña más alta del mundo:'),
       respuesta: new Respuesta(/^\s*everest\s*$/i)
       
     },
     
-    { pregunta: 'Isla más grande del archipiélago Canario',
+    { pregunta: new PreguntaCorta('Isla más grande del archipiélago Canario'),
       respuesta: new Respuesta(/^\s*tenerife\s*$/i)
       
     },
     
-    { pregunta: 'Según la película "Guía del autoestopísta galáctico", ¿Cuál es el sentido de la vida?',
+    { pregunta: new PreguntaCorta('Según la película "Guía del autoestopísta galáctico", ¿Cuál es el sentido de la vida?'),
       respuesta: new Respuesta(42)
     },
     
     { 
-      pregunta: '¿Que famoso dijo la siguiente frase? "Sólo podemos ver poco del futuro, pero lo suficiente para darnos cuenta de que hay mucho que hacer" ',
+      pregunta: new PreguntaCorta('¿Que famoso dijo la siguiente frase? "Sólo podemos ver poco del futuro, pero lo suficiente para darnos cuenta de que hay mucho que hacer" '),
       respuesta: new Respuesta(/^\s*turing\s*$/i)
     },
          
     { 
-      pregunta: '¿De que color es el cielo?',
+      pregunta: new PreguntaCorta('¿De que color es el cielo?'),
       respuesta: new Respuesta(/^\s*azul\s*$/i)
     },  
     
     
     {
-      pregunta: '¿Quien reinaba en España cuando se descubrió América?',
+      pregunta: new PreguntaCorta('¿Quien reinaba en España cuando se descubrió América?'),
       respuesta: new Respuesta(/\b(Isabel\s+y?\s*Fernando)|(Fernando\s+[ey]?\s*Isabel)|(reyes\s+cat[oó]licos)\b/i)
       
     },
@@ -69,21 +90,25 @@ function Quiz() {
     { /* Código inseguro. ¡No ejecute esta pregunta salvo en un
          entorno en el que el código del "alumno" sea fiable!
        */
-      pregunta: 'Escriba una función JavaScript de nombre <tt>square</tt> '+
-      'que recibe un número y devuelve el cuadrado',
-      respuesta: function(x) {
-        try {
-          eval(x); /* DANGER DANGER DANGER */
-          var z = Math.floor(Math.random()*100);
-          return (square(z) == z*z);
-        }
-        catch(e) {
+      pregunta: new PreguntaLarga('Escriba una función JavaScript de nombre <tt>square</tt> '+ 'que recibe un número y devuelve el cuadrado'),
+      respuesta: new Respuesta(
+        function(x) {
+          try {
+            eval(x); /* DANGER DANGER DANGER */
+            var z = Math.floor(Math.random()*100);
+            return (square(z) == z*z);
+          }
+          catch(e) {
+            return false;
+          }
           return false;
         }
-        return false;
-      }
+      )
+      
     }
   );
+  
+  
   // insertar unas cuantas preguntas sobre
   // la tabla de multiplicar
    var self  = this;
@@ -92,7 +117,7 @@ function Quiz() {
       var n1 = Math.randomInt(9)+1;
       var n2 = Math.randomInt(9)+1;
       self.q.push(
-        { pregunta: '¿ '+n1+'x'+n2+"= ?",
+        { pregunta: new PreguntaCorta('¿ '+n1+'x'+n2+"= ?"),
           respuesta: new Respuesta(n1*n2)
       })
     })();
